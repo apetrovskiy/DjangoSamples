286260abd6fc58c3a9886f92665fd67a32ea50ef
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-09-21 12:14:07 +0300

    a temporary version

--- Lightweight/ch01/hello.py
+++ Lightweight/ch01/hello.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+import os
+import sys
 
 
 def index(request):
     return HttpResponse('Hello World')
+
+
+# if __name__ == '__main__':
+#     from django.core.management import execute_from_command_line
+#     execute_from_command_line(sys.argv)
